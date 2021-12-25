1d01c0ca23556c2057fd5889a0044f5a652ef72e
Dolev Dublon <62290677+dolev146@users.noreply.github.com>
2021-12-25 18:34:51 +0200

    tests

--- Tests/test_GraphAlgo.py
+++ Tests/test_GraphAlgo.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 from Classes.GraphAlgo import GraphAlgo
+import os
+
 
 class TestGraphAlgo(TestCase):
     def test_load_from_json(self):
@@ -9,28 +11,44 @@ class TestGraphAlgo(TestCase):
         self.fail()
 
     def test_shortest_path(self):
-        self.fail()
+        script_dir = os.path.dirname(__file__)
+        rel_path = "../data/short1.json"
+        abs_file_path = os.path.join(script_dir, rel_path)
+        ga = GraphAlgo()
+        ga.load_from_json(abs_file_path)
+
 
     def test_plot_graph(self):
-        ga =GraphAlgo()
-        ga.load_from_json("/Users/yakovkhodorkovski/PycharmProjects/Ex3/data/T1.json")
-        ga.plot_graph()
-        self.assertNotEqual(ga.get_graph().get_all_v().get(0).x , None)
-        self.assertNotEqual(ga.get_graph().get_all_v().get(1).x, None)
-        self.assertNotEqual(ga.get_graph().get_all_v().get(2).x, None)
-        self.assertNotEqual(ga.get_graph().get_all_v().get(3).x, None)
-
-        ga.load_from_json("/Users/yakovkhodorkovski/PycharmProjects/Ex3/data/T2.json")
-        ga.plot_graph()
-        self.assertNotEqual(ga.get_graph().get_all_v().get(0).x, None)
-        self.assertNotEqual(ga.get_graph().get_all_v().get(1).x, None)
-        self.assertNotEqual(ga.get_graph().get_all_v().get(2).x, None)
-        self.assertNotEqual(ga.get_graph().get_all_v().get(3).x, None)
-
-        ga.load_from_json("/Users/yakovkhodorkovski/PycharmProjects/Ex3/data/T3.json")
+        """
+        because of the problem with python that needs the absolute path if the file you
+        try to open is in a directory that is not in the Which is not smaller than her in her hierarchy or neighbor
+        in the location of the function activation
+        we use the absolute path for the checks.
+        ** update we learned to use os.path to fix that
+        """
+        script_dir = os.path.dirname(__file__)
+        rel_path = "../data/T1.json"
+        abs_file_path = os.path.join(script_dir, rel_path)
+        ga = GraphAlgo()
+        ga.load_from_json(abs_file_path)
         ga.plot_graph()
         self.assertNotEqual(ga.get_graph().get_all_v().get(0).x, None)
         self.assertNotEqual(ga.get_graph().get_all_v().get(1).x, None)
         self.assertNotEqual(ga.get_graph().get_all_v().get(2).x, None)
         self.assertNotEqual(ga.get_graph().get_all_v().get(3).x, None)
-
+        rel_path = "../data/T2.json"
+        abs_file_path = os.path.join(script_dir, rel_path)
+        ga.load_from_json(abs_file_path)
+        ga.plot_graph()
+        self.assertNotEqual(ga.get_graph().get_all_v().get(0).x, None)
+        self.assertNotEqual(ga.get_graph().get_all_v().get(1).x, None)
+        self.assertNotEqual(ga.get_graph().get_all_v().get(2).x, None)
+        self.assertNotEqual(ga.get_graph().get_all_v().get(3).x, None)
+        rel_path = "../data/T3.json"
+        abs_file_path = os.path.join(script_dir, rel_path)
+        ga.load_from_json(abs_file_path)
+        ga.plot_graph()
+        self.assertNotEqual(ga.get_graph().get_all_v().get(0).x, None)
+        self.assertNotEqual(ga.get_graph().get_all_v().get(1).x, None)
+        self.assertNotEqual(ga.get_graph().get_all_v().get(2).x, None)
+        self.assertNotEqual(ga.get_graph().get_all_v().get(3).x, None)
